3e264e9fa3236f8f72ec301b40e241d6c485ab88
ytakeuchi <hello@ytakeuchi.jp>
2024-10-08 00:46:26 +0900

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    experimental: {
+        largePageDataBytes: 128 * 100000,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "www.pokemon-card.com",
+                pathname: "/**",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
